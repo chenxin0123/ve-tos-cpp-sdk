c32688f20a6824d369e32c58fdf63c767c24b277
chenxin <407008175@qq.com>
2024-06-04 18:12:46 +0800

    cmake批处理

---
+++ build_vs2019.bat
@@ -0,0 +1,12 @@
+mkdir build
+cd build
+mkdir vs2019_64
+cd vs2019_64
+del /s /q ".\CMakeCache.txt"
+cmake ../..  -DBUILD_SHARED_LIB=ON -G "Visual Studio 16 2019" -A x64
+cd ..
+mkdir vs2019_32
+cd vs2019_32
+del /s /q ".\CMakeCache.txt"
+cmake ../..  -DBUILD_SHARED_LIB=ON -G "Visual Studio 16 2019" -A Win32
+cd ..
